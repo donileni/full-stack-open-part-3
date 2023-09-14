87c20f61c884fdfd705b3002a73a0772d33b0397
David <david.markus.olsson@gmail.com>
2023-09-14 20:31:14 +0200

    Finished 3.2

--- index.js
+++ index.js
@@ -29,6 +29,14 @@ app.get('/api/persons', (request, response) => {
   response.json(persons)
 })
 
+app.get('/info', (request, response) => {
+    const entriesText = `Phonebook has info for ${persons.length} people`
+    const now = new Date().toString()
+    const combinedText = `<p>${entriesText}</p><p>${now}</p>`
+
+    response.send(combinedText)
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
