cf938aee272eb72d9f28281eed7339c9da1f863f
David <david.markus.olsson@gmail.com>
2023-11-11 14:06:34 +0100

    Finished 3.16

--- index.js
+++ index.js
@@ -10,14 +10,22 @@ morgan.token("tokenData", (req, res) => {
   return JSON.stringify(req.body);
 });
 
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } 
+
+  next(error)
+}
+
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :tokenData'))
 
 const cors = require('cors')
 
 app.use(cors())
-
 app.use(express.static('dist'))
-
 
 app.get('/api/persons', (request, response) => {
   Person.find({}).then(persons => {
@@ -33,28 +41,23 @@ app.get('/info', (request, response) => {
     response.send(combinedText)
 })
 
-app.get('/api/persons/:id', (request, response) => {
+app.get('/api/persons/:id', (request, response, next) => {
     Person.find({ id: request.params.id }).then(person => {
-      if (person) {
-        response.json(person)
-      }
-      else {
+      if (person.length === 0) {
         response.status(404).end()
       }
+      else {
+        response.json(person)
+      }
     })
-    .catch(error =>{
-      console.log(error)
-      response.status(400).send({error: 'malformatted id'})
-    })
+    .catch(error => next(error))
 })
 
 app.delete('/api/persons/:id', (request, response) => {
     Person.findOneAndDelete({id: request.params.id}).then(result => {
       response.status(204).end()
     })
-    .catch(error =>{
-      console.log(error)
-    })
+    .catch(error => next(error))
 })
 
 const generateId = () => {
@@ -82,6 +85,8 @@ app.post('/api/persons/', (request, response) => {
     response.json(savedPerson)
   })
 })
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
